12e49573ad3f76bfe949c3c9457c710a83fe4507
= <yousefkhalil125@gmail.com>
2019-10-13 14:57:46 +0200

    v 1.0.0

--- build/index.js
+++ build/index.js
@@ -1,29 +1,15 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var engine_1 = __importDefault(require("./graphic/engine"));
-var canvas_1 = __importDefault(require("./graphic/canvas"));
-var primal_1 = require("./geometry/primal");
-var enums_1 = require("./enums");
-var canvas = new canvas_1.default({
-    size: {
-        height: 10,
-        width: 10
-    },
-    blankStyle: {
-        shape: "#",
-        color: enums_1.COLOR_TYPES.CYAN
-    }
-});
-var polygon = new primal_1.Polygon([
-    { x: 0, y: 0 },
-    { x: 0, y: 9 },
-    { x: 9, y: 9 },
-    { x: 9, y: 0 }
-], { color: enums_1.COLOR_TYPES.RED, shape: "*" });
-canvas.draw(polygon.getPixels());
-var engine = new engine_1.default();
-engine.addCanvas({ canvas: canvas });
-engine.render();
+var graphic = __importStar(require("./graphic"));
+exports.graphic = graphic;
+var shapes = __importStar(require("./geometry/primal"));
+exports.shapes = shapes;
+var enums = __importStar(require("./enums"));
+exports.enums = enums;
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,11 @@
-import Engine from "./graphic/engine";
+import * as graphic from "./graphic";
 
-import Canvas from "./graphic/canvas";
+import * as shapes from "./geometry/primal"
 
-import { Polygon } from "./geometry/primal"
+import * as enums from "./enums";
 
-import { 
-    COLOR_TYPES 
-} from "./enums";
-
-const canvas = new Canvas({
-    size: {
-        height: 10,
-        width: 10
-    },
-    blankStyle: {
-        shape: "#",
-        color: COLOR_TYPES.CYAN
-    }
-});
-
-const polygon = new  Polygon([
-    {x: 0, y: 0},
-    {x: 0, y: 9},
-    {x: 9, y: 9},
-    {x: 9, y: 0}
-], {color: COLOR_TYPES.RED, shape: "*"});
-
-canvas.draw(polygon.getPixels());
-
-const engine = new Engine();
-
-engine.addCanvas({canvas});
-
-engine.render();
+export {
+    shapes,
+    graphic,
+    enums
+}
