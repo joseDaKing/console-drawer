0b31307294a372013347e810aa8a8d2c31e18724
= <yousefkhalil125@gmail.com>
2019-10-14 00:46:57 +0200

    v 1.0.1

--- build/index.js
+++ build/index.js
@@ -10,25 +10,3 @@ exports.PolygonalChain = primal_1.PolygonalChain;
 exports.Polygon = primal_1.Polygon;
 var enums_1 = require("./enums");
 exports.COLOR_TYPES = enums_1.COLOR_TYPES;
-var polygon = new primal_1.Polygon([
-    { x: 0, y: 0 },
-    { x: 0, y: 20 },
-    { x: 20, y: 20 }
-], { shape: "*", color: enums_1.COLOR_TYPES.GREEN });
-polygon.translate({ x: 15, y: 7 });
-var canvas = new graphic_1.Canvas({
-    size: {
-        height: 35,
-        width: 50
-    },
-    blankStyle: {
-        shape: "#",
-        color: enums_1.COLOR_TYPES.BLUE
-    }
-});
-canvas.draw(polygon.getPixels());
-var engine = new graphic_1.Engine();
-engine.addCanvas({
-    main: canvas
-});
-engine.render();
--- src/index.ts
+++ src/index.ts
@@ -23,33 +23,3 @@ export {
     Polygon,
     COLOR_TYPES
 }
-
-
-const polygon = new Polygon([
-    {x: 0, y: 0},
-    {x: 0, y: 20},
-    {x: 20, y: 20}
-], {shape: "*", color: COLOR_TYPES.GREEN});
-
-polygon.translate({x: 15, y: 7});
-
-const canvas = new Canvas({
-    size: {
-        height: 35,
-        width: 50
-    },
-    blankStyle: {
-        shape: "#",
-        color: COLOR_TYPES.BLUE
-    }
-});
-
-canvas.draw(polygon.getPixels());
-
-const engine = new Engine();
-
-engine.addCanvas({
-    main: canvas
-});
-
-engine.render();
